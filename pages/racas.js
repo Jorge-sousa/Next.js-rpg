f437623d5045fa774967aaca0532b5e9da36298a
Jorge de Sousa <63168978+Jorge-sousa@users.noreply.github.com>
2021-02-13 21:05:15 -0300

    Adicionado link raças

---
+++ pages/racas.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+function Racas(){
+    return(<div>
+        <h1>Raças</h1>
+        <Link href="/">
+        <a>Retornar para página princial.</a>
+        </Link>
+    </div>)
+}
+export default Racas
